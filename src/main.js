05c86a89053b153b79043e0ca98678684917f5f3
gchris2024 <gchris3586@gmail.com>
2026-03-09 09:49:19 +0100

    Create game store with state and getters

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+import { useGameStore } from "./stores/gameStore";
+
 import App from "./App.vue";
 import router from "./router";
 import "./style.css";
@@ -8,3 +10,4 @@ const app = createApp(App);
 app.use(createPinia());
 app.use(router);
 app.mount("#app");
+useGameStore();
